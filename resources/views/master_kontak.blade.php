991fa8e1bccb3481417dfae6813128e03c6a11c0
rahmaa <woirahma@gmail.com>
2022-10-24 14:20:43 +0700

    push utama

---
+++ resources/views/master_kontak.blade.php
@@ -0,0 +1,8 @@
+@extends('admin')
+ 
+@section('title', 'Master Kontak')
+@section('content-title', 'Master Kontak')
+@section('content')
+    <h6>Halaman Kontak</h6>
+    
+@endsection
---
+++ resources/views/tambah_kontak.blade.php
@@ -0,0 +1,8 @@
+@extends('admin')
+ 
+@section('title', 'Tambah Kontak')
+@section('content-title', 'Tambah Kontak')
+@section('content')
+
+    
+@endsection
